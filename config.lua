9f71acc1d3ed228fa6060d56094f2abc4bf965b5
k3vyn21 <75340723+k3vyn21@users.noreply.github.com>
2024-05-22 16:30:42 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Showers = {
+	{coords = vector3(-1391.36, -1334.95, 4.15)},
+	{coords = vector3(-1386.02, -1335.65, 4.15)},
+	{coords = vector3(-1386.05, -1320.19, 4.15)},
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Script para ir al baño y generar otros ambientes al PJ'
+autor 'k3vyn21'
+version '1.0'
+
+server_scripts {
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
